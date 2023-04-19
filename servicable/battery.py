d58e386161147b534dcbea1b03459794bb3d5e25
proof3 <124drak@gmail.com>
2023-04-19 15:46:31 +0400

    Update Spindler battery time to service

--- servicable/battery.py
+++ servicable/battery.py
@@ -15,7 +15,7 @@ class SpindlerBattery(Battery):
         self.current_date = current_date
 
     def needs_service(self) -> bool:
-        return (self.current_date - self.last_service_date).days > 365 * 2
+        return (self.current_date - self.last_service_date).days > 365 * 3
     
 class NubbinBattery(Battery):
     def __init__(self, last_service_date: date, current_date: date) -> None:
--- test/test_battery/test_spindler.py
+++ test/test_battery/test_spindler.py
@@ -5,12 +5,12 @@ from servicable.battery import SpindlerBattery
 class TestSpindlerBattery(unittest.TestCase):
     def test_battery_should_be_serviced(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 3)
+        last_service_date = today.replace(year=today.year - 4)
         battery = SpindlerBattery(last_service_date, today)
         self.assertTrue(battery.needs_service())
 
     def test_battery_should_not_be_serviced(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 2)
+        last_service_date = today.replace(year=today.year - 3)
         battery = SpindlerBattery(last_service_date, today)
         self.assertFalse(battery.needs_service())
